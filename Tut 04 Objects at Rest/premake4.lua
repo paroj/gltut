93388d9f1ddef55139d9eae238975417273c6b48
Sokka <unknown>
2010-05-04 16:32:44 -0700

    Tutorial 4 mostly done. Still needs glossary and images.

--- Tut 04 Objects at Rest/premake4.lua
+++ Tut 04 Objects at Rest/premake4.lua
@@ -8,3 +8,5 @@ SetupProject("ShaderPerspective", "ShaderPerspective.cpp",
 	"data/StandardColors.frag", "data/ManualPerspective.vert")
 SetupProject("MatrixPerspective", "MatrixPerspective.cpp",
 	"data/StandardColors.frag", "data/MatrixPerspective.vert")
+SetupProject("AspectRatio", "AspectRatio.cpp",
+	"data/StandardColors.frag", "data/MatrixPerspective.vert")
