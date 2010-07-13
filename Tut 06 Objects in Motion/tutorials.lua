c522e6ed7cdf6d23ccd65e7f3d7efc751cd95e13
Alfonse <unknown>
2010-07-13 14:12:43 -0700

    Modified the Tutorial 6 projects.

--- Tut 06 Objects in Motion/tutorials.lua
+++ Tut 06 Objects in Motion/tutorials.lua
@@ -1,5 +1,7 @@
 
-SetupProject("Translation Scale", "TranslationScale.cpp",
+SetupProject("Translation", "Translation.cpp",
+	"data/ColorPassthrough.frag", "data/PosColorLocalTransform.vert")
+SetupProject("Scale", "Scale.cpp",
 	"data/ColorPassthrough.frag", "data/PosColorLocalTransform.vert")
 SetupProject("Rotations", "Rotations.cpp",
 	"data/ColorPassthrough.frag", "data/PosColorLocalTransform.vert")
