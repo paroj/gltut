d0171bc571a385f4230c2ed2b057597e371571b9
Alfonse <unknown>
2012-02-23 10:40:48 -0800

    Copyediting, and adjustment to TOC generation for FO processing.

--- Documents/Build/_commonFOParams.lua
+++ Documents/Build/_commonFOParams.lua
@@ -2,6 +2,6 @@ local params = ...;
 params = params or {};
 
 params["fop1.extensions"] = "1";
-params["generate.toc"] = "appendix  toc,title article/appendix  nop article   toc,title book      toc,title chapter   toc,title part      toc,title preface   toc,title qandadiv  toc qandaset  toc reference toc,title sect1     toc sect2     toc sect3     toc sect4     toc sect5     toc section   toc set       toc,title"
+params["generate.toc"] = "book      toc,title"
 
 return params;
